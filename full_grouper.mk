3b7831f707db97a5392e90da23ab7251a6767633
dhacker29 <davidhackerdvm@gmail.com>
2012-12-14 11:11:55 -0600

    Add fingerprint
    Change-Id: I236ae217d8d235c3a7614d299217706846476f71

--- full_grouper.mk
+++ full_grouper.mk
@@ -28,8 +28,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full Android on Grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
 
 # Don't restrict vendor folder
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2 JOP40C 527662 release-keys"
